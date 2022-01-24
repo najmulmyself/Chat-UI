400b5e4379b771c3d0181fad8200d55d621a844f
Najmul Huda <najmul.myself@gmail.com>
2022-01-24 23:21:54 +0600

    class declared

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,11 +1,57 @@
 class User {
   int? id;
   String? name;
-  String? imgUrl;
+  String? imageUrl;
 
   User({
     this.id,
     this.name,
-    this.imgUrl,
+    this.imageUrl,
   });
 }
+
+final User currentUser = User(
+  id: 0,
+  name: 'Current User',
+  imageUrl: 'assets/images/greg.jpg',
+);
+
+// USERS
+final User greg = User(
+  id: 1,
+  name: 'Greg',
+  imageUrl: 'assets/images/greg.jpg',
+);
+final User james = User(
+  id: 2,
+  name: 'James',
+  imageUrl: 'assets/images/james.jpg',
+);
+final User john = User(
+  id: 3,
+  name: 'John',
+  imageUrl: 'assets/images/john.jpg',
+);
+final User olivia = User(
+  id: 4,
+  name: 'Olivia',
+  imageUrl: 'assets/images/olivia.jpg',
+);
+final User sam = User(
+  id: 5,
+  name: 'Sam',
+  imageUrl: 'assets/images/sam.jpg',
+);
+final User sophia = User(
+  id: 6,
+  name: 'Sophia',
+  imageUrl: 'assets/images/sophia.jpg',
+);
+final User steven = User(
+  id: 7,
+  name: 'Steven',
+  imageUrl: 'assets/images/steven.jpg',
+);
+
+// FAVORITE CONTACTS
+List<User> favorites = [sam, steven, olivia, john, greg];
