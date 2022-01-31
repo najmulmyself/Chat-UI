d904d8fb8b2d19f27f31e53a0395ac60e19141db
Najmul Huda <najmul.myself@gmail.com>
2022-01-31 21:09:32 +0600

    horizontal category selection

--- lib/main.dart
+++ lib/main.dart
@@ -18,4 +18,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-// fixed emulator issue
+
--- lib/widgets/category_selector.dart
+++ lib/widgets/category_selector.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Category_selector extends StatelessWidget {
-  const Category_selector({
-    Key? key,
-  }) : super(key: key);
+  // const Category_selector({
+  //   Key? key,
+  // }) : super(key: key);
+
+  int selectedIndex = 0;
+  List categories = ['Messages', 'Online', 'Group', 'Requests'];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 90,
-      color: Colors.black,
+      color: Colors.blue,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 32),
+            child: Text(categories[index]),
+          );
+        },
+        itemCount: categories.length,
+      ),
     );
   }
 }
